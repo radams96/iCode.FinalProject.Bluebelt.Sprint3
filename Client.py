8b49ca6000ef4b77d0ddbecbc209864ef7fca2ac
radams96 <38706589+radams96@users.noreply.github.com>
2018-11-10 10:50:18 -0600

    Update Client.py
    Co-authored-by:RobertLeonReynolds <robertleonreynolds@gmail.com>

--- Client.py
+++ Client.py
@@ -1,20 +1,20 @@
 import socket                                   #Import socket features.
 import time                                     #Import time fetaures.
 
-s = socket.socket()                             #Create a socket.
+mySocket = socket.socket()                      #Create a socket.
 host = "W.X.Y.Z"                                #Set the host IP of the intended connection.
 port = 12345                                    #Set port to 12345, a common port for program testing.
-s.connect(host, port)                           #Connect to the given IP through the given port.
+mySocket.connect((host,port))                    #Connect to the given IP through the given port.
 
-while true:                                     #Loop constantly.
+while True:                                     #Loop constantly.
     response = raw_input("Send Message?")       #Get user input.
     if(response == "yes"):                      #If yes, send a message of 'Y'.
-        s.send('Y')
+        mySocket.send('Y')
         time.sleep(3)
     elif(response == "no"):                     #If no, send message 'N' and terminate the loop.
-        s.send('N')
+        mySocket.send('N')
         break
-    else                                        #Invalid response catch-all.
+    else:                                        #Invalid response catch-all.
         print("Invalid response.")
         time.sleep(3)
 
