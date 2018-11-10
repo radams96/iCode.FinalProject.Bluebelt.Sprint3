46c3b30656dfcb9eea20093403a8bb05256bde6b
radams96 <38706589+radams96@users.noreply.github.com>
2018-11-10 10:56:54 -0600

    Update Server.py
    Co-authored-by: RobertLeonReynolds <robertleonreynolds@gmail.com>

--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
 import socket                               #Import socket features.
 import time                                 #Import time features.
-import RPi.GPIo as GPIO                     #Import GPIO features, and name GPIO.
+import RPi.GPIO as GPIO                     #Import GPIO features, and name GPIO.
 
 GPIO.setmode(GPIO.BOARD)                    #Set GPIO numbering convention to BOARD.
 GPIO.setup(11, GPIO.OUT)                    #Set pin 11 as GPIO output.
@@ -12,16 +12,16 @@ def led_blink():                        #DEFINING THE LED BLINK FUNCTION.
     GPIO.output(11,False)                   #Turn off LED.
     time.sleep(1)                           #Wait for one second.
 
-s = socket.socket()                         #Create a socket.
+mySocket = socket.socket()                  #Create a socket.
 host = "W.X.Y.Z"                            #Set the host IP of the intended connection.
 port = 12345                                #Set port to 12345, a common port for program testing.
-s.bind(host,port)                           #Bind to the given IP through the given port.
+mySocket.bind((host,port))                  #Bind to the given IP through the given port.
 print "Listening..."                        #Display to user that the device is listening.
-s.listen(5)                                 #Perform listen function.
-c, addr = s.accept()                        #Accept message transmition.
+mySocket.listen(5)                         #Perform listen function.
+connection = mySocket.accept()              #Accept message transmition.
 
 while True:
-    message = c.recv(1024)                  #Receive message and store it.
+    message = connection.recv(1024)         #Receive message and store it.
     if(message == 'Y'):                     #If message is 'Y', run led_blink()
         led_blink()
     elif(message == 'N'):                   #If message is 'N', break the loop.
@@ -29,5 +29,5 @@ while True:
         break
     else:                                   #Otherwise, print invalid message received.
         print "No message received."
-c.close()                                   #Close the socket.
+connection.close()                          #Close the socket.
 GPIO.cleanup()                              #Cleanup the GPIO.
